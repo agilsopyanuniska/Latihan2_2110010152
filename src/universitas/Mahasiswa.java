169281f5070df42035adeb46169b3221ac83f04f
agilsopyanuniska <agilsopyan54@gmail.com>
2023-06-13 08:59:56 +0700

    Melengkapi class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -7,4 +7,35 @@ public class Mahasiswa {
     public String getDetail(){
         return nama + npm + prodi;
     }
+
+    public Mahasiswa(String npm, String nama, String prodi) {
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
 }
